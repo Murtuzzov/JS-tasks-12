c9624f7f25c5bf8f14f956128f2bd54135f2953e
murtuzzov <murtuzzov>
2024-03-11 23:52:22 +0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,34 @@
+//П Е Р В А Я   Ч А С Т Ь
+
+//ЗАДАНИЕ No1 | создать объект, описывающий автомобиль (производитель,
+// модель, год выпуска, средняя скорость), и следующие функции
+// для работы с этим объектом.
+// car = {
+//   manufacturer: "Japan",
+//   model: "Tiida",
+//   dateOfMade: 2012,
+//   middleSpeed: 60,
+// };
+
+//ЗАДАЧА No1 ПЕРВОГО ЗАДАНИЯ | функция для вывода на экран информации об автомобиле.
+
+//   function carInfo(carData){
+//     const{country, model, dateOfMade, middleSpeed} = carData;
+//     return Производитель:${country}, Модель:${model}, Год выпуска:${dateOfMade}, Средняя скорость:${middleSpeed}
+//   }
+//   const result = carInfo(car)
+//   console.log(result)
+
+//ЗАДАЧА No2 ПЕРВОГО ЗАДАНИЯ | функция для подсчета необходимого времени
+// для преодоления переданного расстояния со средней скоростью.
+// Учтите, что через каждые 4 часа дороги водителю необходимо делать перерыв на 1 час.
+
+// function distanceFn(distance) {
+//   let time = distance / car.middleSpeed;
+//   let breakCounter = Math.floor(time / 4);
+//   let totalTime = time + breakCounter;
+//   return `Автомобиль преодолеет путь за ${totalTime.toFixed(1)} часов.`;
+// }
+
+// let result = distanceFn(500);
+// console.log(result);
